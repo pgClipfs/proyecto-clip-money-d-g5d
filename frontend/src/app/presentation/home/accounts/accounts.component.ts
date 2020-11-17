55546c8b191c91c8abc149b1ab2676ee7d9f6889
JoaMoreno <tec.ignaciomoreno@gmail.com>
2020-11-16 21:51:42 -0300

    added accounts components

---
+++ frontend/src/app/presentation/home/accounts/accounts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts',
+  templateUrl: './accounts.component.html',
+  styleUrls: ['./accounts.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AccountsComponent implements OnInit {
+
+  @Input() accounts: any[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
