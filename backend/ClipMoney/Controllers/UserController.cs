cccc9918fd2e974fbef62ad5d19a21bf06846c00
Santiago Garcia <santiago.garcia@kdsoft.io>
2021-01-25 18:59:01 -0300

    Se agrega get de usuario y put de perfil

---
+++ backend/ClipMoney/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using ClipMoney.BusinessLogic;
+using ClipMoney.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClipMoney.Controllers
+{
+    [Route("users")]
+    [ApiController]
+    public class UserController: ControllerBase
+    {
+        private readonly UserBusinessLogic _userBusinessLogic;
+
+        public UserController(UserBusinessLogic userBusinessLogic)
+        {
+            _userBusinessLogic = userBusinessLogic;
+        }
+
+        /// <summary>
+        /// Get user by id in database and return the user model.
+        /// </summary>
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _userBusinessLogic.GetUserById(id);
+            
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// Edit user profile.
+        /// </summary>
+        [HttpPut]
+        public IActionResult EditProfile(UserModel user)
+        {
+            var newProfile = _userBusinessLogic.EditProfile(user);
+
+            return Ok(newProfile);
+        }
+
+    }
+}
---
+++ backend/ClipMoney/Models/ProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClipMoney.Models
+{
+    public class ProfileModel
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
